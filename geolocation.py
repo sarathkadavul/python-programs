cbb2d133b13933faf2d03fe9baf17f9f5f1a53e4
sarathkadavul <sksarath48@gmail.com>
2017-07-13 21:02:34 +0530

    Added comments for understanding the flow of code

--- geolocation.py
+++ geolocation.py
@@ -1,9 +1,14 @@
+#uses google maps api
+#pip install googlemaps for installing googlemaps api module
 import googlemaps
 
+#gets the address
 location = raw_input("Enter the address")
 
+#you can generate your own key from developer.google.com
 gmkey = googlemaps.Client(key="AIzaSyAxa5bAMnE913kjuPSFz2xDxTvo_4X6OKc")
 
+#requests for geocode function with location as parameter
 geoloc = gmkey.geocode(location)
 
 filter1 = geoloc[0]
