3c4415fa5b988be9444f2bdf180627a496015538
sarathkadavul <sksarath48@gmail.com>
2021-01-14 20:58:07 +0530

    Added one liner to text to speech

--- text_to_speech.py
+++ text_to_speech.py
@@ -4,3 +4,5 @@ from gtts import gTTS
 in_text = input("Enter text ")
 audio=gTTS(text=in_text,lang='en',slow=False)
 audio.save("audio.mp3")
+
+#one liner: gTTS(text=input("Enter text "),lang='en',slow=False).save("audio.mp3")
